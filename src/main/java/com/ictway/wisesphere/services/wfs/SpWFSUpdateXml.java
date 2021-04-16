d0362beaa9ab3df21c7e6335abe2ffaea20c55d4
DESKTOP-H7VNKF0\22cun <22cun2@naver.com>
2021-04-16 15:48:03 +0900

    2021-04-16

--- src/main/java/com/ictway/wisesphere/services/wfs/SpWFSUpdateXml.java
+++ src/main/java/com/ictway/wisesphere/services/wfs/SpWFSUpdateXml.java
@@ -2,6 +2,7 @@ package com.ictway.wisesphere.services.wfs;
 
 import static org.deegree.commons.xml.jaxb.JAXBUtils.unmarshall;
 import static org.deegree.services.controller.OGCFrontController.getServiceWorkspace;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,13 +23,18 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.deegree.commons.config.DeegreeWorkspace;
+import org.deegree.layer.persistence.LayerStore;
+import org.deegree.layer.persistence.LayerStoreManager;
 import org.deegree.theme.persistence.standard.StandardThemeProvider;
 import org.deegree.theme.persistence.standard.jaxb.Themes;
+import org.slf4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 public class SpWFSUpdateXml {
+	
+	private static final Logger LOG = getLogger(SpWFSUpdateXml.class);
 	
 	private static final URL SCHEMA_URL = StandardThemeProvider.class.getResource( "/META-INF/schemas/themes/3.2.0/themes.xsd" );
 	
@@ -63,11 +69,21 @@ public class SpWFSUpdateXml {
 		Themes cfg = (Themes) unmarshall( pkg, SCHEMA_URL, configUrl, workspace );
 		List<String> layers = cfg.getLayerStoreId();
 		
+		//유효성 검사를 위한 코드
+		LayerStoreManager mgr = workspace.getSubsystemManager( LayerStoreManager.class );
+		
 		//xml에 featureStoreId 추가
+		//21.0.16 유효성 검사 추가
 		for(String layer : layers) {
-			Element feature = doc.createElement("FeatureStoreId");
-			feature.setTextContent(layer);
-			root.insertBefore(feature, root.getElementsByTagName("EnableTransactions").item(0)); //EnableTransactions 앞부분에 추가
+			LayerStore store = mgr.get(layer);
+			if(store != null) {
+				Element feature = doc.createElement("FeatureStoreId");
+				feature.setTextContent(layer);
+				root.insertBefore(feature, root.getElementsByTagName("EnableTransactions").item(0)); //EnableTransactions 앞부분에 추가
+			}else {
+				LOG.warn( "Layer store with LayerStoreId {} is not available.", layer );
+				continue;
+			}
 		}
 		
 		//xml 쓰기
